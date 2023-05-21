2a759373409d1f3a5a690743b6074c80506f38b7
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-21 15:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+window.onload = function()
+{
+      var line = document.getElementById("line");
+      var angle = 0; // Initial angle
+      var speed = 1; // Rotation speed
+      function rotateLine() {
+        line.style.transform = "rotate(" + angle + "deg)";
+        angle += speed;
+        requestAnimationFrame(rotateLine);
+      }
+      rotateLine();
+    };
